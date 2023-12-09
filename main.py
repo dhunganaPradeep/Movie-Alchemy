ee5cb1ff3974ee9bbe902c1e6f822d18fd2f73b6
dhunganaPradeep <pradeep.dhungana23@gmail.com>
2023-12-09 19:12:55 +0545

    last commit

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pickle
 import streamlit as st
-import pandas as pd
-import os
-# import ast
 import requests
 import base64
+import time
+from random import randint
 
 st.set_page_config(page_title='MOVIE ALCHEMY', page_icon=':smiley:')
 
@@ -12,32 +11,27 @@ st.set_page_config(page_title='MOVIE ALCHEMY', page_icon=':smiley:')
 movies = pickle.load(open('movie_list.pkl', 'rb'))
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-
 # Data Preprocessing
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    data = requests.get(url).json()
     poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
-
+    return f"https://image.tmdb.org/t/p/w500/{poster_path}"
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movies = []
     for i in distances[1:6]:
-        if i[0] < len(movies):  # Check if the index is within bounds
+        if i[0] < len(movies):
             movie_details = movies.iloc[i[0]]
-            recommended_movie = {   
+            recommended_movie = {
                 'title': movie_details['title'],
                 'poster': fetch_poster(movie_details['movie_id']),
                 'overview': " ".join(movie_details['overview']),
                 'cast': ", ".join(movie_details['cast']),
             }
             recommended_movies.append(recommended_movie)
-
     return recommended_movies
 
 # Streamlit app
@@ -60,8 +54,8 @@ selected_movie = st.sidebar.selectbox(
 )
 show_recommendations = st.sidebar.button('Show Recommendations')
 
+# Check if recommendations should be displayed
 if show_recommendations and selected_movie:
-    # Show recommendations only if 'Show Recommendations' button is clicked and a movie is selected
     st.title(f'List of recommended movies for {selected_movie}')
     recommended_movies = recommend(selected_movie)
 
@@ -74,3 +68,33 @@ if show_recommendations and selected_movie:
             st.write("-------------------------------")
     else:
         st.write("No recommendations found.")
+
+# Movie Posters
+st.header('You may Like')
+num_posters = 4
+
+with st.container():
+    poster_container = st.container()
+
+    # Open a new row
+    with poster_container:
+        columns = st.columns(num_posters)
+
+        for i in range(num_posters):
+            movie = movies.iloc[i]
+            poster_url = fetch_poster(movie['movie_id'])
+            columns[i].write(
+                f'<img src="{poster_url}" width="170" style="cursor: pointer; margin-bottom: 20px;" class="poster">'
+                , unsafe_allow_html=True)
+
+# Featured movies section
+st.header('Featured Movies')
+featured_movies = ['Inception', 'The Shawshank Redemption', 'The Dark Knight', 'Pulp Fiction', 'Fight Club']
+
+for movie_title in featured_movies:
+    movie_details = movies[movies['title'] == movie_title].iloc[0]
+    st.write(f"## {movie_title}")
+    st.image(fetch_poster(movie_details['movie_id']), width=200)
+    st.write(f"**Overview:** {' '.join(movie_details['overview'])}")
+    st.write(f"**Cast:** {', '.join(movie_details['cast'])}")
+    st.write("---")
